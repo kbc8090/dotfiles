09a57cb9da6fc3fde206e50526800f013caaf100
kbc8090 <63003083+kbc8090@users.noreply.github.com>
2024-08-07 17:41:10 -0400

    Add files via upload

--- config.fish
+++ config.fish
@@ -2,13 +2,14 @@
 
 set -U fish_greeting
 
-set TERM "xterm-256color"
-set LIBGL_ALWAYS_SOFTWARE "true"
-set GALLIUM_DRIVER "llvmpipe kitty"
+set TERM "alacritty"
+# set LIBGL_ALWAYS_SOFTWARE "true"
+# set GALLIUM_DRIVER "llvmpipe kitty"
 set EDITOR "nvim"
 set BROWSER "chromium"
 set BAT_THEME "base16-256"
 set -gx EXA_ICON_SPACING "2"
+set -gx EZA_ICON_SPACING "2"
 # set -x LESS "-X"
 set fish_color_normal cyan
 set fish_color_autosuggestion black
@@ -43,9 +44,10 @@ set -x MANPAGER "nvim +Man!"
 # set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
 # set -g __fish_git_prompt_color_cleanstate green --bold
 #
-alias ls='exa -a --group-directories-first --icons --color=always'
-alias ll='exa -alh --group-directories-first --git --icons --color=always'
+alias ls='eza -a --group-directories-first --icons --color=always'
+alias ll='eza -alh --group-directories-first --git --icons --color=always'
 alias vim='$EDITOR'
+alias btop='btop -lc'
 alias v='$EDITOR'
 alias neovim='nvim'
 alias df='df -h'
@@ -58,7 +60,8 @@ alias rg='rg -S'
 alias fd='fd -H'
 alias bat='bat --theme="base16-256"'
 alias du='du -h'
-alias sdn='shutdown now'
+alias sdn='sudo poweroff'
+alias reboot='sudo reboot'
 alias pacup='sudo pacman -Syyu'
 alias fishy='$EDITOR $HOME/.config/fish/config.fish'
 alias config='cd $HOME/.config'
@@ -101,10 +104,14 @@ alias cstar='$EDITOR $HOME/.config/starship.toml'
 # end
 
 function fish_title
-  printf "%s" "$USER"
-  printf "@"
-  printf "%s" "$hostname: "
-  printf "%s" "$PWD"
+  echo $USER@$hostname: (prompt_pwd)
 end
 
 starship init fish | source
+
+# Start X at login
+# if status is-login
+#   if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+#     exec startx -- -keeptty
+#   end
+# end
